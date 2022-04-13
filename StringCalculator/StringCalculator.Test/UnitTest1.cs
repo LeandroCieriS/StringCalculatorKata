e0c491c93783d247b1db6d9c57fe62588f654fbf
Leandro Cieri <lcieri@domingoalonsogroup.com>
2022-04-13 07:58:31 +0100

    Red: Return0WhenEmptyString

--- StringCalculator/StringCalculator.Test/UnitTest1.cs
+++ StringCalculator/StringCalculator.Test/UnitTest1.cs
@@ -6,64 +6,28 @@ namespace StringCalculator.Test
 {
     public class StringCalculatorShould
     {
-        private StringCalculator stringCalculator;
 
         [SetUp]
         public void Setup()
         {
-            stringCalculator = new StringCalculator();
+            
         }
 
         [Test]
         public void Return0WhenEmptyString()
         {
+            var stringCalculator = new StringCalculator();
 
-            Assert.AreEqual(0, stringCalculator.Add(""));
+            Assert.AreEqual("0", stringCalculator.Add(""));
         }
 
-        [Test]
-        [TestCase(1, "1")]
-        [TestCase(2, "2")]
-        [TestCase(3, "3")]
-        public void ReturnNumberWhenInputIsSingleNumber(int expected, string input)
-        {
-
-            Assert.AreEqual(expected, stringCalculator.Add(input));
-        }
-
-        [Test]
-        [TestCase(3, "1,2")]
-        [TestCase(5, "2,3")]
-        [TestCase(7, "3,4")]
-        public void ReturnAdditionOfTwoNumbers(int expected, string input)
-        {
-
-            Assert.AreEqual(expected, stringCalculator.Add(input));
-        }
-
-        [Test]
-        public void ReturnAdditionOfAnyNumbers()
-        {
-
-            Assert.AreEqual(4, stringCalculator.Add("1,2,1"));
-        }
-
-        [Test]
-        public void UseNewLineAsSeparator()
-        {
-            Assert.AreEqual(4, stringCalculator.Add("1,2\n1"));
-        }
     }
 
     public class StringCalculator
     {
         public int Add(string input)
         {
-            if (input == "")
-                return 0;
-            input = input.Replace("\n", ",");
-            var splitInput = input.Split(",");
-            return splitInput.Select(int.Parse).Sum();
+            throw new NotImplementedException();
         }
     }
 }
